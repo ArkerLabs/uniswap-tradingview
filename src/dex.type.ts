086f3463a0a2394236e9022e23923b2edd47f8af
Juanjo Chust <nytyr@me.com>
2022-03-31 18:02:47 +0200

    feat: 🎸 Add pancake

--- src/dex.type.ts
+++ src/dex.type.ts
@@ -1,4 +1,5 @@
 export enum Dex {
     UNISWAP = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2', 
-    QUICKSWAP = 'https://api.thegraph.com/subgraphs/name/sameepsi/quickswap'
+    QUICKSWAP = 'https://api.thegraph.com/subgraphs/name/sameepsi/quickswap', 
+    PANCAKESWAP = 'https://api.thegraph.com/subgraphs/name/mmontes11/pancakeswap'
 }
